fd722edb1a482e432798610666588a346ea4e1df
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2021-03-24 21:36:40 +0800

    added error page

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,18 @@ module Aldwin7894
     config.time_zone = "Singapore"
     config.active_record.default_timezone = :local
 
+    config.exception_handler = {
+      dev:        false, # allows you to turn ExceptionHandler "on" in development
+      db:         nil, # allocates a "table name" into which exceptions are saved (defaults to nil)
+      email:      nil, # sends exception emails to a listed email (string // "you@email.com")
+
+      exceptions: {
+        all: {
+          layout: "exception", # define layout
+        }
+      }
+    }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
