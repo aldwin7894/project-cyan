cafaa514a39531cc4ab478406e4db8148562f89b
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2021-10-19 19:42:52 +0800

    Added lograge

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  config.lograge.enabled = Rails.configuration.x.feature.lograge
+  config.lograge.base_controller_class = "ActionController::API"
+
+  config.lograge.formatter = Lograge::Formatters::Json.new
+  config.lograge.base_controller_class = ["ActionController::API", "ActionController::Base"]
+
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w[controller action format id]
+    {
+      request_time: Time.zone.now.strftime("%B %e, %Y %r"),
+      application: Rails.application.class.parent_name,
+      process_id: Process.pid,
+      host: event.payload[:host],
+      remote_ip: event.payload[:remote_ip],
+      ip: event.payload[:ip],
+      x_forwarded_for: event.payload[:x_forwarded_for],
+      headers: JSON.generate(
+        event.payload[:headers].env.select do |k|
+          k.match("^HTTP.*|^CONTENT.*|^AUTHORIZATION.*")
+        end
+      ),
+      params: event.payload[:params].except(*exceptions).to_json,
+      rails_env: Rails.env,
+      exception: event.payload[:exception]&.first,
+      request_id:     event.payload[:headers]["action_dispatch.request_id"],
+      exception_message: "'#{event.payload[:exception]&.last}'",
+      exception_backtrace: event.payload[:exception_object]&.backtrace&.join(","),
+    }.compact
+  end
+end
