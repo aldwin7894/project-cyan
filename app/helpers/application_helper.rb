baa2d7126e81f511439af199763106a27352fc00
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2021-10-24 02:40:55 +0800

    Convert images to base64

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def asset_data_uri(path)
+    asset = Rails.application.assets.find_asset path
+
+    throw "Could not find asset '#{path}'" if asset.nil?
+
+    base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
+    "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
+  end
 end
