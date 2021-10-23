c4c4089f876fd5db513d97c69aeb1fb3428d5d95
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2021-10-24 02:40:55 +0800

    Convert images to base64

--- app/controllers/lastfm_controller.rb
+++ app/controllers/lastfm_controller.rb
@@ -7,6 +7,13 @@ class LastfmController < ApplicationController
     @recent = LASTFM_CLIENT.user.get_recent_tracks(user: params[:username], limit: 2, extended: 1).first
     @background = params[:bg]
     @foreground = params[:fg]
+    if @recent&.[]("image")&.[](3)&.[]("content").present?
+      img = HTTParty.get(@recent["image"][3]["content"])
+      base64 = Base64.encode64(img.to_s).gsub(/\s+/, "")
+      @album_art = "data:image/jpg;base64,#{Rack::Utils.escape(base64)}"
+    else
+      @album_art = asset_data_uri "lastfm-placeholder.png"
+    end
 
     respond_to do |format|
       format.png do
