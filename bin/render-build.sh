43ee615dc74d1cad187c368b4505e88af6adc9c3
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2021-12-20 15:56:35 +0800

    Fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+yarn install
 BUNDLE_WITHOUT='development:test' BUNDLE_DEPLOYMENT=1 bundle install -j4
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
