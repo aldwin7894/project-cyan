f78a1428d79642014c71a4b6ae8633a1a541a4eb
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-01-18 23:34:51 +0800

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+echo "deb http://pub-repo.sematext.com/ubuntu sematext main" | sudo tee /etc/apt/sources.list.d/sematext.list > /dev/null
+wget -O - https://pub-repo.sematext.com/ubuntu/sematext.gpg.key | sudo apt-key add -
+sudo apt-get -y update
+sudo apt-get -y install sematext-agent
+
+sudo bash /opt/spm/bin/setup-infra \
+    --infra-token 00cc46cd-a1ad-444c-a07d-13295e49fd79
+
 yarn install --frozen-lockfile
 BUNDLE_WITHOUT='development:test' BUNDLE_DEPLOYMENT=1 bundle install -j4
 bundle exec rake assets:precompile
