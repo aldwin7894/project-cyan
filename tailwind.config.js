7c81a6982a418552c91e99a822fcfde9155c8873
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-08-01 00:44:59 +0800

    Adjusted responsiveness and added additional links

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,17 @@ module.exports = {
   darkMode: "media", // or 'class'
   theme: {
     screens: {
-      xs: "475px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
+      xs: { min: "375px", max: "639px" },
+      sm: { min: "640px", max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: { min: "1024px", max: "1279px" },
+      xl: { min: "1280px", max: "1535px" },
+      "2xl": { min: "1536px" },
+      phone: "375px",
+      tablet: "576px",
+      laptop: "992px",
+      desktop: "1200px",
+      "desktop-lg": "1640px",
     },
     colors: ({ colors }) => ({
       primary: "#0B1622",
