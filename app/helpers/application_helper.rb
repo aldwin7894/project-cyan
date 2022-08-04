d5d7a83306f2cec62cc388681b9a8aa5c1c4c457
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-08-04 14:02:04 +0800

    Inline vite assets on lastfm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def image_url_to_base64(url)
+    album_art = nil
+    img = Rails.cache.fetch(url, expires_in: 7.days, skip_nil: true) do
+      HTTParty.get(url, format: :plain, headers: { "Accept-Encoding" => "gzip,deflate,br" }).body
+    end
+
+    if img.present?
+      base64 = Base64.strict_encode64(img).gsub(/\s+/, "")
+      album_art = "data:image/#{File.extname(url).strip.downcase[1..-1]};base64,#{Rack::Utils.escape(base64)}"
+    end
+
+    album_art
+  end
 end
