10bea87915544c9790d3061aeb46254777117905
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-08-04 12:37:23 +0800

    CORS tryfix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,16 +3,30 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV.fetch("ASSETS_ALLOWED_ORIGINS").split(" ")
+    resource "/assets/*",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
     resource "/vite/*",
       headers: :any,
-      methods: [:get, :options, :head]
+      methods: [:get, :post, :options, :head]
     resource "/vite/assets/*",
       headers: :any,
-      methods: [:get, :options, :head]
+      methods: [:get, :post, :options, :head]
   end
 
   allow do
     origins "*"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head]
+    resource "/*.png",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    resource "/*.ico",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    resource "/*.html",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    resource "/robots.txt",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
   end
 end
