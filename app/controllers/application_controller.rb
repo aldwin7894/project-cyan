34fb52e390230744783898100c15a31730782d06
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-08-07 20:42:31 +0800

    Prevent cdn from accessing routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@
 require "anilist/anilist"
 
 class ApplicationController < ActionController::Base
+  before_action :check_if_from_cloudfront
   class QueryError < StandardError; end
 
   private
+    def check_if_from_cloudfront
+      raise "Operation not possible." if request.headers["HTTP_USER_AGENT"] == "Amazon CloudFront"
+    end
     def query(definition, variables = {})
       response = AniList::Client.query(definition, variables:)
 
