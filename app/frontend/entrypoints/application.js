a66ef835411c5895bfe9f8e6b5a84cd482c2a0ca
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-09-25 04:13:47 +0800

    Revamped profile section

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -3,6 +3,7 @@ import * as ActiveStorage from "@rails/activestorage";
 import Alpine from "alpinejs";
 import "iconify-icon";
 import Chart from "chart.js/auto";
+import "tw-elements";
 import "../channels";
 import "~/stylesheets/application.css.scss";
 
@@ -11,3 +12,8 @@ ActiveStorage.start();
 window.Alpine = Alpine;
 window.Chart = Chart;
 Alpine.start();
+
+const popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]'),
+);
+popoverTriggerList.map((popoverTriggerEl) => new window.Popover(popoverTriggerEl));
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       md: { min: "768px", max: "1023px" },
       lg: { min: "1024px", max: "1279px" },
       xl: { min: "1280px", max: "1535px" },
-      "2xl": { min: "1536px" },
+      "2xl": { min: "1536px", max: "1791px" },
       phone: "0px",
       tablet: "576px",
       laptop: "992px",
