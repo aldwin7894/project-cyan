25cc13dcba935804b0c4ac8986f3c030350e12ed
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-09-25 20:59:34 +0800

    Smoothen out loading and transitions

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -17,3 +17,20 @@ const popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]'),
 );
 popoverTriggerList.map((popoverTriggerEl) => new window.Popover(popoverTriggerEl));
+
+// fade animations
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementsByTagName("body")[0];
+  element.classList.add("animate__fadeIn", "animate__animated");
+
+  element.addEventListener("animationend", () => {
+    element.classList.remove("animate__fadeIn", "animate__animated");
+  });
+});
+document.addEventListener("turbo:before-fetch-response", (event) => {
+  const element = document.getElementById(event.target.id);
+  element.classList.add("animate__fadeIn", "animate__animated", "animate__delay");
+  element.addEventListener("animationend", () => {
+    element.classList.remove("animate__fadeIn", "animate__animated", "animate__delay");
+  });
+});
