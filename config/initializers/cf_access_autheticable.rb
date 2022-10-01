2c269ff87878249de3e02e0f1091a10ab95b239f
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-10-01 14:34:11 +0800

    Move cms to subdomain, and add cf access strategy to devise

---
+++ config/initializers/cf_access_autheticable.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+## This class extends devise to validate the JWT provided by cloudflare and ensure the email addresses match
+module Devise
+  module Strategies
+    class CfAccessAuthenticatable < Authenticatable
+      def authenticate!
+        token = request.cookies["CF_Authorization"]
+
+        unless token.present?
+          Rails.logger.info("JWT Cookie not found")
+          redirect!(cf_teams_url)
+        end
+
+        jwk_loader = lambda do |options|
+          @cached_keys = nil if options[:invalidate]
+          @cached_keys ||= keys
+        end
+
+        payload, = JWT.decode(token, nil, true, {
+                                nbf_leeway: 30, # allowed drift in seconds
+                                exp_leeway: 30, # allowed drift in seconds
+                                iss: cf_teams_url,
+                                verify_iss: true,
+                                aud: cf_aud,
+                                verify_aud: true,
+                                verify_iat: true,
+                                algorithm: "RS256",
+                                jwks: jwk_loader
+                              })
+        email = payload["email"]
+
+        resource = User.find_by(email:)
+        unless resource
+          Rails.logger.info("User #{email} not found")
+          redirect!(cf_teams_url)
+        end
+
+        remember_me(resource)
+        resource.after_database_authentication
+        success!(resource)
+      rescue JWT::DecodeError => e
+        Rails.logger.error(e.message)
+        redirect!(cf_teams_url)
+      end
+
+      private
+        def cf_aud
+          ENV.fetch("CF_JWT_AUD")
+        end
+
+        def cf_teams_url
+          ENV.fetch("CF_TEAMS_URL")
+        end
+
+        def keys
+          @keys ||= HTTParty.get("#{cf_teams_url}/cdn-cgi/access/certs").deep_symbolize_keys!
+        end
+    end
+  end
+end
