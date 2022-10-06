c916b6e5d1192663d1bb71e82201ca6ee11453ba
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-10-06 16:52:36 +0800

    Fixed s3 error

--- app/models/concerns/attachment_movable.rb
+++ app/models/concerns/attachment_movable.rb
@@ -15,12 +15,12 @@ module AttachmentMovable
     def process_image
       # Optimize image only if the existing key doesn't
       # have S3_BUCKET_ENV on it, meaning it has been uploaded.
-      unless blob.key.include?(S3_BUCKET_ENV) && blob.image?
-        new_key = "web/#{S3_BUCKET_ENV}/uploads/#{self.record_type.downcase}/#{blob.key}"
+      # unless blob.key.include?(S3_BUCKET_ENV) && blob.image?
+      #   new_key = "web/#{S3_BUCKET_ENV}/uploads/#{self.record_type.downcase}/#{blob.key}"
 
-        # optimize image and upload to new key
-        optimized = optimize_image(blob)
-        move_to_uploads(blob, new_key, optimized)
-      end
+      #   # optimize image and upload to new key
+      #   optimized = optimize_image(blob)
+      #   move_to_uploads(blob, new_key, optimized)
+      # end
     end
 end
