c32894ebcbd7cf5792452737389e9c4ba9ad8839
aldwin7894 <14094252+aldwin7894@users.noreply.github.com>
2022-10-21 20:05:27 +0800

    Fixed image_url_to_base64 helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,28 +4,30 @@ require "addressable/uri"
 
 module ApplicationHelper
   def image_url_to_base64(url)
-    album_art = nil
-    if url.blank? then return album_art end
+    image = nil
+    return image if url.blank?
+
+    IMAGE_FILETYPES = ["jpg", "jpeg", "gif", "png", "webp"]
     filetype = Addressable::URI.parse(url).extname.remove(".")
 
     img = Rails.cache.fetch(url, expires_in: 1.month, skip_nil: true) do
       data = {}
       res = HTTParty.get(url, format: :plain, headers: { "Accept-Encoding" => "gzip,deflate,br" })
       ext = res.headers.content_type.split("/")
-      if ext[0] == "image" || (ext[0] == "text" && filetype == "webp")
+      if ext[0] == "image" || IMAGE_FILETYPES.include?(filetype)
         data[:image] = res.body
         data[:ext] = ext[1]
       end
       if %w(jpg jpeg gif png).include?(data[:ext]) && ENV.fetch("OPTIMIZE_IMAGE_ENABLED", false)
         data[:image] = Rails.configuration.x.image_optim.optimize_image_data(data[:image].to_s)
       end
-      data.present? ? data : nil
+      data.presence ? data : nil
     end
 
     if img&.[](:image).present?
       base64 = Base64.strict_encode64(img[:image]).gsub(/\s+/, "")
-      album_art = "data:image/#{img[:ext]};base64,#{Rack::Utils.escape(base64)}"
+      image = "data:image/#{img[:ext]};base64,#{Rack::Utils.escape(base64)}"
     end
-    album_art
+    image
   end
 end
