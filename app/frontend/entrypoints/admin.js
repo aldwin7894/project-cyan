74616d261deb137130c76f532d9a056130ad25e3
aldwin7894 <cyan@aldwin7894.win>
2023-05-19 01:25:17 +0800

    Fix vite build

--- app/frontend/entrypoints/admin.js
+++ app/frontend/entrypoints/admin.js
@@ -1,11 +1,8 @@
 import "iconify-icon";
 import "@hotwired/turbo-rails";
-import * as ActiveStorage from "@rails/activestorage";
 import Alpine from "alpinejs";
 import "../channels";
 import "~/stylesheets/admin.css.scss";
-
-ActiveStorage.start();
 
 window.Alpine = Alpine;
 Alpine.start();
