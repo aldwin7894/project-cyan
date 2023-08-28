3ae26e5e4535f79f616bd459f943b6eea802c14f
aldwin7894 <cyan@aldwin7894.win>
2023-08-28 13:46:51 +0800

    Add env to disable signoz telemetry

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -2,6 +2,8 @@ require "opentelemetry/sdk"
 require "opentelemetry/exporter/otlp"
 require "opentelemetry/instrumentation/all"
 
-OpenTelemetry::SDK.configure do |c|
-  c.use_all
+if ENV.fetch("SIGNOZ_ENABLED", "false") == "true"
+  OpenTelemetry::SDK.configure do |c|
+    c.use_all
+  end
 end
