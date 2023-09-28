709bb11e1de40e7b90f753fb559114d1a050b579
aldwin7894 <cyan@aldwin7894.win>
2023-09-28 19:15:55 +0800

    Moved to ruby-lsp and sorbet

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,9 +1,9 @@
-require "opentelemetry/sdk"
-require "opentelemetry/exporter/otlp"
-require "opentelemetry/instrumentation/all"
+# require "opentelemetry/sdk"
+# require "opentelemetry/exporter/otlp"
+# require "opentelemetry/instrumentation/all"
 
-if ENV.fetch("SIGNOZ_ENABLED", "false") == "true"
-  OpenTelemetry::SDK.configure do |c|
-    c.use_all
-  end
-end
+# if ENV.fetch("SIGNOZ_ENABLED", "false") == "true"
+#   OpenTelemetry::SDK.configure do |c|
+#     c.use_all
+#   end
+# end
