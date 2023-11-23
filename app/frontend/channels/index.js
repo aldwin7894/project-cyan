57747c5b6ce3002353e194a54fa41981563f123d
aldwin7894 <cyan@aldwin7894.win>
2023-11-23 21:01:18 +0800

    Update dependencies

--- app/frontend/channels/index.js
+++ app/frontend/channels/index.js
@@ -1,5 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = import.meta.globEager("./*_channel.js");
+const channels = import.meta.glob("./*_channel.js", { eager: true });
 Object.keys(channels).forEach(path => channels[path]());
