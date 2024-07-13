fbc55a4413e24eac3237870644bbe3640e68de8d
aldwin7894 <cyan@aldwin7894.dev>
2024-07-13 23:23:08 +0800

    Add hasNextPage

--- lib/anilist.rb
+++ lib/anilist.rb
@@ -161,6 +161,9 @@ module AniList
     query($user_id: Int!, $page: Int, $per_page: Int, $date: Int) {
       Page(page: $page, perPage: $per_page) {
         activities(userId: $user_id, createdAt_greater: $date, sort: ID, type: ANIME_LIST) {
+          pageInfo {
+            hasNextPage
+          }
           ... on ListActivity {
             id
           }
